c5dce8e7c4743eda8c3d5f5cf3e9c8913af371be
shaxadhere <shaxad.here@gmail.com>
2025-04-11 02:59:14 +0500

    added project grid responsiveness, removed footer, added exp page

---
+++ src/constants/source.constants.js
@@ -0,0 +1,5 @@
+export const SOURCE = {
+    DEPLOY: "https://vercel.com/new/clone?repository-url=https://github.com/shaxadhere/portfolio-v4",
+    GITHUB: "https://github.com/shaxadhere/portfolio-v4",
+    FORK: "https://github.com/shaxadhere/portfolio-v4/fork"
+}
